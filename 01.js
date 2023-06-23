df8926b9d8191fed035d43aaa3bac7be3e7980d5
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-06-23 16:09:27 +0530

    execute

--- 01.js
+++ 01.js
@@ -1,26 +1,10 @@
 // global execution
 // local execution
 // closures 
-// console.log(this);
-// console.log(window);
-// console.log(firstName);
-// var firstName = "Dinesh";
+console.log(this);
+console.log(window);
+console.log(firstName);
+var firstName = "Dinesh";
+console.log(firstName);
 
-
-let age=[12,33,24,19,28,25,55];
-
-// let votable=age.filter(myFunc);
-
-// function myFunc(age){
-//     return age>18
-// }
-
-// console.log(votable);
-
-
-let votable=age.filter((age)=>{
-    return age>18
-});
-
-console.log(votable);
 
