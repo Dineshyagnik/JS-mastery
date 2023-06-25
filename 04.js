ce060e03a22c7d73f3f451400ce2d42e6b433ac0
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-06-25 16:08:43 +0530

    05.js

---
+++ 04.js
@@ -0,0 +1,12 @@
+//  Uncaught ReferenceError: Cannot access 'firstName' before initialization
+
+// console.log(firstName);
+// let firstName="John";
+// console.log(firstName);
+
+
+// Uncaught ReferenceError:firstName is not defined
+// console.log(firstName);
+
+console.log(typeof firstName);
+let firstName='john';
---
+++ 05.js
@@ -0,0 +1,10 @@
+console.log("Hello world!!");
+let firstName="John";
+let lastName="Doe";
+
+const myFunction = function(){
+    let var1="First Variable";
+    let var2="Second Variable";
+    console.log(var1);
+    console.log(var2);
+}
