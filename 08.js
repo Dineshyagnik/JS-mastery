75a0dbaa7649480c21b196a01cae32fa4b19d6fd
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-06-28 21:04:04 +0530

    08.js

---
+++ 08.js
@@ -0,0 +1,11 @@
+// const age=[22,12,15,17,19,32,25,26,29];
+// let votable=age.filter((age)=>{
+//     return age>18
+// });
+// console.log(votable);
+
+let arr=[22,12,23,11,22,32];
+let arr2=arr.map((item)=>{
+    return item*2;
+});
+console.log(arr2);
