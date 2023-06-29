2b43b34236626ab7103e76889db873646c64611f
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-06-29 16:42:59 +0530

    08.js

--- 08.js
+++ 08.js
@@ -4,8 +4,31 @@
 // });
 // console.log(votable);
 
-let arr=[22,12,23,11,22,32];
-let arr2=arr.map((item)=>{
-    return item*2;
-});
-console.log(arr2);
+// let arr=[22,12,23,11,22,32];
+// let arr2=arr.map((item)=>{
+//     return item*2;
+// });
+// console.log(arr2);
+
+// const number = prompt('Enter Number');
+
+// if(number>0){
+//     console.log('The number is positive.');
+// }
+// else{
+//     console.log('The number is negative.');
+// }
+
+// program to display numbers from 1 to 5
+// const n = prompt('enter number')
+
+// // looping from i = 1 to 5
+// // in each iteration, i is increased by 1
+// for (let i = 1; i <= n; i++) {
+//     console.log(i);     // printing the value of i
+// }
+
+
+
+
+
