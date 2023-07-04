35b078183618fb7dc095e71bab0dcee0c00d64d6
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-07-04 17:03:32 +0530

    hoisting

--- 08.js
+++ 08.js
@@ -259,16 +259,66 @@
 
 // console.log(a+b); //error
 
-let a= 'Hello';
+// let a= 'Hello';
 
-function greet(){
-    let b='world';
-    console.log(a+' '+b);
+// function greet(){
+//     let b='world';
+//     console.log(a+' '+b);
 
-    if(b=='world'){
-        let c='hello';
-        console.log(a+' '+b+' '+c);
-    }
-    console.log(a+' '+b+' '+c); //error
+//     if(b=='world'){
+//         let c='hello';
+//         console.log(a+' '+b+' '+c);
+//     }
+//     console.log(a+' '+b+' '+c); //error
+// }
+// greet();
+
+// using test before declaring
+// var test;
+// console.log(test); // undefined
+
+// program to display value
+// a = 5;
+// console.log(a);
+// var a; // 5
+
+
+// program to display value
+// var a;
+// a = 5;
+// console.log(a); // 5
+
+// program to display value
+// console.log(a);
+// var a = 5; //undefined
+
+// program to display value
+// var a = 4;
+
+// function greet() {
+//     b = 'hello';
+//     console.log(b); // hello
+//     var b;
+// }
+
+// greet(); // hello
+// console.log(b); // error
+
+// program to display value
+// a = 5;
+// console.log(a);
+// let a; // error
+
+// program to print the text
+// greet();// Hi, there.
+
+// function greet() {
+//     console.log('Hi, there.');
+// }
+
+// program to print the text
+greet(); // error
+
+let greet = function() {
+    console.log('Hi, there.');
 }
-greet();
