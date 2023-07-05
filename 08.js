43f143223f9e7e2ac163d400c975ce1442286139
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-07-05 12:55:21 +0530

    recursion

--- 08.js
+++ 08.js
@@ -317,8 +317,48 @@
 // }
 
 // program to print the text
-greet(); // error
+// greet(); // error
 
-let greet = function() {
-    console.log('Hi, there.');
+// let greet = function() {
+//     console.log('Hi, there.');
+// }
+
+
+// program to count down numbers to 1
+// function countDown(number) {
+
+//     // display the number
+//     console.log(number);
+
+//     // decrease the number value
+//     const newNumber = number - 1;
+
+//     // base case
+//     if (newNumber > 0) {
+//         countDown(newNumber);
+//     }
+// }
+
+// countDown(4); //4321
+
+// program to find the factorial of a number
+function factorial(x) {
+
+    // if number is 0
+    if (x === 0) {
+        return 1;
+    }
+
+    // if number is positive
+    else {
+        return x * factorial(x - 1);
+    }
+}
+
+const num = 3;
+
+// calling factorial() if num is non-negative
+if (num > 0) {
+    let result = factorial(num);
+    console.log(`The factorial of ${num} is ${result}`);
 }
