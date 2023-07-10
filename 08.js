dab66fc09ee2b70014bd21ad11c42566f72a09ca
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-07-10 15:43:17 +0530

    getter and setter

--- 08.js
+++ 08.js
@@ -558,6 +558,56 @@
 
 // console.log(student.firstName); // Sarah
 
+// const student = {
+//     firstName: 'Monica'
+// }
+
+// // getting property
+// Object.defineProperty(student, "getName", {
+//     get : function () {
+//         return this.firstName;
+//     }
+// });
+
+// // setting property
+// Object.defineProperty(student, "changeName", {
+//     set : function (value) {
+//         this.firstName = value;
+//     }
+// });
+
+// console.log(student.firstName); // Monica
+
+// // changing the property value
+// student.changeName = 'Sarah';
+
+// console.log(student.firstName); // Sarah
+
+
+// const student ={
+//     firstName:'monica',
+
+//     get getName(){
+//         return this.firstName;
+//     }
+// };
+
+// console.log(student.firstName);
+// console.log(student.getName);
+
+// const student={
+//     firstName:'Monica',
+
+//     set changeName(newName){
+//         this.firstName=newName;
+//     }
+// }
+// console.log(student.firstName);
+
+// student.changeName='sarah';
+
+// console.log(student.firstName);
+
 const student = {
     firstName: 'Monica'
 }
