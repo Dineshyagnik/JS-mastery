e940f66e16b30bcaaa899c6d6694dc4d1bfe1dcf
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-07-15 17:54:25 +0530

    loop

--- 08.js
+++ 08.js
@@ -935,26 +935,81 @@
 // console.log(typeof a); // "string"
 // console.log(typeof b); // "object"
 
-const text1 = 'hello';
-const text2 = 'world';
-const text3 = '     JavaScript    ';
+// const text1 = 'hello';
+// const text2 = 'world';
+// const text3 = '     JavaScript    ';
 
-// concatenating two strings
-const result1 = text1.concat(' ', text2);
-console.log(result1); // "hello world"
+// // concatenating two strings
+// const result1 = text1.concat(' ', text2);
+// console.log(result1); // "hello world"
 
-// converting the text to uppercase
-const result2 = text1.toUpperCase();
-console.log(result2); // HELLO
+// // converting the text to uppercase
+// const result2 = text1.toUpperCase();
+// console.log(result2); // HELLO
 
-// removing whitespace from the string
-const result3 = text3.trim();
-console.log(result3); // JavaScript
+// // removing whitespace from the string
+// const result3 = text3.trim();
+// console.log(result3); // JavaScript
 
-// converting the string to an array
-const result4 = text1.split();
-console.log(result4); // ["hello"]
+// // converting the string to an array
+// const result4 = text1.split();
+// console.log(result4); // ["hello"]
 
-// slicing the string
-const result5= text1.slice(1, 3);
-console.log(result5); // "el"
+// // slicing the string
+// const result5= text1.slice(1, 3);
+// console.log(result5); // "el"
+
+
+// const a = 225; // number
+// const b = true; // boolean
+
+// //converting to string
+// const result1 = String(a);
+// const result2 = String(b);
+
+// console.log(result1); // "225"
+// console.log(result2); // "true"
+
+// const student = {
+//     name: 'Monica',
+//     class: 7,
+//     age: 12
+// }
+
+// // using for...in
+// for ( let key in student ) {
+
+//     // display the properties
+//     console.log(`${key} => ${student[key]}`);
+// }
+
+// const salaries= {
+//     Jack : 24000,
+//     Paul : 34000,
+//     Monica : 55000
+// }
+
+// // using for...in
+// for ( let i in salaries) {
+
+//     // add a currency symbol
+//     let salary = "$" + salaries[i];
+
+//     // display the values
+//     console.log(`${i} : ${salary}`);
+// }
+
+// const string = 'code';
+
+// // using for...in loop
+// for (let i in string) {
+//     console.log(string[i]);
+// }
+
+// define array
+const arr = [ 'hello', 1, 'JavaScript' ];
+
+// using for...in loop
+for (let x in arr) {
+    console.log(arr[x]);
+}
