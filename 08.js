af930d67a9c7d7d2c5f16ca1b69a69358b226446
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-07-20 15:04:13 +0530

    arrow function

--- 08.js
+++ 08.js
@@ -1312,10 +1312,86 @@
 // show('one','two','three','four','five','six');
 
 
-let arr1=['one','two'];
-// let arr2=[...arr1,'three','four','five'];
-// console.log(arr2);
-let arr2=['three','four','five'];
+// let arr1=['one','two'];
+// // let arr2=[...arr1,'three','four','five'];
+// // console.log(arr2);
+// let arr2=['three','four','five'];
 
-let arr3=[...arr1,...arr2];
-console.log(arr3);
+// let arr3=[...arr1,...arr2];
+// console.log(arr3);
+
+
+// let greet = () => console.log('Hello');
+// greet(); // Hello
+
+// let greet = x => console.log(x);
+// greet('Hello'); // Hello 
+
+// let age = 5;
+
+// let welcome = (age < 18) ?
+//   () => console.log('Baby') :
+//   () => console.log('Adult');
+
+// welcome(); // Baby
+
+// let sum = (a, b) => {
+//     let result = a + b;
+//     return result;
+// }
+
+// let result1 = sum(5,7);
+// console.log(result1); // 12
+
+// function Person() {
+//     this.name = 'Jack',
+//     this.age = 25,
+//     this.sayName = function () {
+
+//         // this is accessible
+//         console.log(this.age);
+
+//         function innerFunc() {
+
+//             // this refers to the global object
+//             console.log(this.age);
+//             console.log(this);
+//         }
+
+//         innerFunc();
+
+//     }
+// }
+
+// let x = new Person();
+// x.sayName();
+
+// function Person() {
+//     this.name = 'Jack',
+//     this.age = 25,
+//     this.sayName = function () {
+
+//         console.log(this.age);
+//         let innerFunc = () => {
+//             console.log(this.age);
+//         }
+
+//         innerFunc();
+//     }
+// }
+
+// const x = new Person();
+// x.sayName();
+
+let person = {
+    name: 'Jack',
+    age: 25,
+    sayName: () => {
+
+        // this refers to the global .....
+        //
+        console.log(this.age);
+    }
+}
+
+person.sayName(); // undefined
