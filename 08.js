d36ab6aacc640e1ff4b80fa5cc98fa365920d9ed
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-07-23 15:18:48 +0530

    spread operator

--- 08.js
+++ 08.js
@@ -1464,20 +1464,82 @@
 
 // console.log(`${result < 10 ? 'Too low': 'Very high'}`)
 
-const name = 'Jack';
-const greet = true;
+// const name = 'Jack';
+// const greet = true;
 
-function tagExample(strings, nameValue) {
-    let str0 = strings[0]; // Hello
-    let str1 = strings[1]; // , How are you?
+// function tagExample(strings, nameValue) {
+//     let str0 = strings[0]; // Hello
+//     let str1 = strings[1]; // , How are you?
 
-    if(greet) {
-        return `${str0}${nameValue}${str1}`;
-    }
+//     if(greet) {
+//         return `${str0}${nameValue}${str1}`;
+//     }
+// }
+
+// // creating tagged literal
+// // passing argument name
+// const result = tagExample`Hello ${name}, How are you?`;
+
+// console.log(result);
+
+
+// const arrValue = ['My', 'name', 'is', 'Jack'];
+
+// console.log(arrValue);   // ["My", "name", "is", "Jack"]
+// console.log(...arrValue); // My name is Jack
+
+// const arr1 = ['one', 'two'];
+// const arr2 = [...arr1, 'three', 'four', 'five'];
+
+// console.log(arr2); 
+// //  Output:
+// //  ["one", "two", "three", "four", "five"]
+
+// let arr1 = [ 1, 2, 3];
+// let arr2 = arr1;
+
+// console.log(arr1); // [1, 2, 3]
+// console.log(arr2); // [1, 2, 3]
+
+// // append an item to the array
+// arr1.push(4);
+
+// console.log(arr1); // [1, 2, 3, 4]
+// console.log(arr2); // [1, 2, 3, 4]
+
+// let arr1 = [ 1, 2, 3];
+
+// // copy using spread syntax
+// let arr2 = [...arr1];
+
+// console.log(arr1); // [1, 2, 3]
+// console.log(arr2); // [1, 2, 3]
+
+// // append an item to the array
+// arr1.push(4);
+
+// console.log(arr1); // [1, 2, 3, 4]
+// console.log(arr2); // [1, 2, 3]
+
+// const obj1 = { x : 1, y : 2 };
+// const obj2 = { z : 3 };
+
+// // add members obj1 and obj2  to obj3
+// const obj3 = {...obj1, ...obj2};
+
+// console.log(obj3); // {x: 1, y: 2, z: 3}
+
+// let func = function(...args) {
+//     console.log(args);
+// }
+
+// func(3); // [3]
+// func(4, 5, 6); // [4, 5, 6]
+
+function sum(x, y ,z) {
+    console.log(x + y + z);
 }
 
-// creating tagged literal
-// passing argument name
-const result = tagExample`Hello ${name}, How are you?`;
+const num1 = [1, 3, 4, 5];
 
-console.log(result);
+sum(...num1); // 8
