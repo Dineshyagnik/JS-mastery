a3341d6939d7acc230ddaf3f39d6a11bc02bb929
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-08-01 14:38:15 +0530

    inheritance

--- 08.js
+++ 08.js
@@ -2006,21 +2006,84 @@
 //   console.log(person1.name); // Sarah
 
 
+// // parent class
+// class Person { 
+//     constructor(name) {
+//         this.name = name;
+//     }
+
+//     greet() {
+//         console.log(`Hello ${this.name}`);
+//     }
+// }
+
+// // inheriting parent class
+// class Student extends Person {
+
+// }
+
+// let student1 = new Student('Jack');
+// student1.greet();
+
+
+// parent class
+// class Person { 
+//     constructor(name) {
+//         this.name = name;
+//     }
+
+//     greet() {
+//         console.log(`Hello ${this.name}`);
+//     }
+// }
+
+// // inheriting parent class
+// class Student extends Person {
+
+//     constructor(name) {
+    
+//         console.log("Creating student class");
+        
+//         // call the super class constructor and pass in the name parameter
+//         super(name);
+//     }
+
+// }
+
+// let student1 = new Student('Jack');
+// student1.greet();
+
 // parent class
 class Person { 
     constructor(name) {
         this.name = name;
+        this.occupation = "unemployed";
     }
-
+    
     greet() {
-        console.log(`Hello ${this.name}`);
+        console.log(`Hello ${this.name}.`);
     }
+ 
 }
 
 // inheriting parent class
 class Student extends Person {
 
+    constructor(name) {
+        
+        // call the super class constructor and pass in the name parameter
+        super(name);
+        
+        // Overriding an occupation property
+        this.occupation = 'Student';
+    }
+    
+    // overriding Person's method
+    greet() {
+        console.log(`Hello student ${this.name}.`);
+        console.log('occupation: ' + this.occupation);
+    }
 }
 
-let student1 = new Student('Jack');
-student1.greet();
+let p = new Student('Jack');
+p.greet();
