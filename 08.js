a9c240086f74b34a101a0afa7c66e7098435570d
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-12-25 22:03:21 +0530

    slice

--- 08.js
+++ 08.js
@@ -2835,30 +2835,35 @@
 // });
 // console.log(sumAll);
 
-let day;
-switch (new Date().getDay()) {
-    case 0:
-        day = 'Sunday';
-        break;
-    case 1:
-        day = 'Monday';
-        break;
-    case 2:
-        day = 'Tuesday';
-        break;
-    case 3:
-        day = 'Wednesday';
-        break;
-    case 4:
-        day = 'Thursday';
-        break;
-    case 5:
-        day = 'Friday';
-        break;
-    case 6:
-        day = 'Saturday';
-        break;
-    default:
-        day = 'not a day';
-};
-console.log(day);
+// let day;
+// switch (new Date().getDay()) {
+//     case 0:
+//         day = 'Sunday';
+//         break;
+//     case 1:
+//         day = 'Monday';
+//         break;
+//     case 2:
+//         day = 'Tuesday';
+//         break;
+//     case 3:
+//         day = 'Wednesday';
+//         break;
+//     case 4:
+//         day = 'Thursday';
+//         break;
+//     case 5:
+//         day = 'Friday';
+//         break;
+//     case 6:
+//         day = 'Saturday';
+//         break;
+//     default:
+//         day = 'not a day';
+// };
+// console.log(day);
+
+
+let arr=[12,134,44,55,667,7,89,9];
+console.log(arr.slice(1));
+console.log(arr.slice(1,2));
