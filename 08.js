f4fea7166b033d24776abd375d90fc0ac4e2b94a
Dinesh Kumar <dyyayavar7491@gmail.com>
2024-01-18 22:51:12 +0530

    callback with settimeout

--- 08.js
+++ 08.js
@@ -3086,16 +3086,31 @@ let data=[
 
 
 
-// function
-function greet(name, callback) {
-    console.log('Hi' + ' ' + name);
-    callback();
+// // function
+// function greet(name, callback) {
+//     console.log('Hi' + ' ' + name);
+//     callback();
+// }
+
+// // callback function
+// function callMe() {
+//     console.log('I am callback function');
+// }
+
+// // passing function as an argument
+// greet('Peter', callMe);
+
+
+//  program that shows the delay in execution
+
+function greet() {
+    console.log('Hello world');
 }
 
-// callback function
-function callMe() {
-    console.log('I am callback function');
+function sayName(name) {
+    console.log('Hello' + ' ' + name);
 }
 
-// passing function as an argument
-greet('Peter', callMe);
+// calling the function
+setTimeout(greet, 2000);
+sayName('John');
