965b5d01873157bb3120d252f862413b367922ae
Dinesh Kumar <dyyayavar7491@gmail.com>
2024-01-20 22:42:00 +0530

    callback function

--- 08.js
+++ 08.js
@@ -3103,14 +3103,31 @@ let data=[
 
 //  program that shows the delay in execution
 
-function greet() {
+// function greet() {
+//     console.log('Hello world');
+// }
+
+// function sayName(name) {
+//     console.log('Hello' + ' ' + name);
+// }
+
+// // calling the function
+// setTimeout(greet, 2000);
+// sayName('John');
+
+// Callback Function Example
+function greet(name, myFunction) {
     console.log('Hello world');
+
+    // callback function
+    // executed only after the greet() is executed
+    myFunction(name);
 }
 
+// callback function
 function sayName(name) {
     console.log('Hello' + ' ' + name);
 }
 
-// calling the function
-setTimeout(greet, 2000);
-sayName('John');
+// calling the function after 2 seconds
+setTimeout(greet, 2000, 'John', sayName);
