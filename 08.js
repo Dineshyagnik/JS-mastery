c065b23ba9af3d5e12b441597bab67dbdce75503
Dinesh Kumar <dyyayavar7491@gmail.com>
2024-01-27 11:33:15 +0530

    positive numbers using while loop

--- 08.js
+++ 08.js
@@ -3214,19 +3214,29 @@ let data=[
 // console.log('sum:', sum);
 
 
+// let sum = 0;
+
+// // take input from the user
+// let number = parseInt(prompt('Enter a number: '));
+
+// while(number >= 0) {
+
+//     // add all positive numbers
+//     sum += number;
+
+//     // take input again if the number is positive
+//     number = parseInt(prompt('Enter a number: '));
+// }
+
+// // display the sum
+// console.log(`The sum is ${sum}.`);
+
 let sum = 0;
+let number = 0;
 
-// take input from the user
-let number = parseInt(prompt('Enter a number: '));
-
-while(number >= 0) {
-
-    // add all positive numbers
+do {
     sum += number;
-
-    // take input again if the number is positive
     number = parseInt(prompt('Enter a number: '));
-}
+} while(number >= 0)
 
-// display the sum
 console.log(`The sum is ${sum}.`);
