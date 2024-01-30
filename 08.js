f140d0509d4803ee34f5f3f12b0c27be2f80a29b
Dinesh Kumar <dyyayavar7491@gmail.com>
2024-01-30 23:17:34 +0530

    break statement

--- 08.js
+++ 08.js
@@ -3242,11 +3242,31 @@ let data=[
 // console.log(`The sum is ${sum}.`);
 
 
-// program to print the value of i
-for (let i = 1; i <= 5; i++) {
-    // break condition     
-    if (i == 3) {
+// // program to print the value of i
+// for (let i = 1; i <= 5; i++) {
+//     // break condition     
+//     if (i == 3) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+let sum = 0, number;
+
+while(true) {
+
+    // take input again if the number is positive
+    number = parseInt(prompt('Enter a number: '));
+
+    // break condition
+    if(number < 0) {
         break;
     }
-    console.log(i);
+
+    // add all positive numbers
+    sum += number;
+
 }
+
+// display the sum
+console.log(`The sum is ${sum}.`);
