2ff453046298b641adff011134c1319027192d86
Dinesh Kumar <dyyayavar7491@gmail.com>
2024-02-11 20:51:53 +0530

    Iterate Over Map Keys

--- 08.js
+++ 08.js
@@ -3375,11 +3375,21 @@ let data=[
 // map1.delete('info'); // true
 // console.log(map1); // Map {}
 
+// let map1 = new Map();
+// map1.set('name', 'Jack');
+// map1.set('age', '27');
+
+// // looping through Map
+// for (let [key, value] of map1) {
+//     console.log(key + '- ' + value);
+// }
+
+
 let map1 = new Map();
 map1.set('name', 'Jack');
 map1.set('age', '27');
 
-// looping through Map
-for (let [key, value] of map1) {
-    console.log(key + '- ' + value);
+// looping through the Map
+for (let key of map1.keys()) {
+  console.log(key)
 }
