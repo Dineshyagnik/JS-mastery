242bbe1829296d5ec2b03eb9a5025267f4bdd0de
Dinesh Kumar <dyyayavar7491@gmail.com>
2024-02-13 22:47:48 +0530

    weakmap

--- 08.js
+++ 08.js
@@ -3395,10 +3395,27 @@ let data=[
 // }
 
 
+// const weakMap = new WeakMap();
+// console.log(weakMap); 
+
+// let obj = {};
+// weakMap.set(obj, 'hello');
+
+// console.log(weakMap);
+
+
 const weakMap = new WeakMap();
 console.log(weakMap); 
 
 let obj = {};
+
 weakMap.set(obj, 'hello');
+
+console.log(weakMap); 
+console.log(weakMap.get(obj)); 
+
+console.log(weakMap.has(obj)); 
+
+console.log(weakMap.delete(obj));
 
 console.log(weakMap);
