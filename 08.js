d67b745ff1d44cc159ae999f14cd376864418198
Dinesh Kumar <dyyayavar7491@gmail.com>
2024-02-24 20:13:42 +0530

    promise with catch

--- 08.js
+++ 08.js
@@ -3557,17 +3557,37 @@ let data=[
 
 // returns a promise
 
+// let countValue = new Promise(function (resolve, reject) {
+//   resolve("Promise resolved");
+// });
+
+// // executes when promise is resolved successfully
+
+// countValue
+//   .then(function successValue(result) {
+//     console.log(result);
+//   })
+
+//   .then(function successValue1() {
+//     console.log("You can call multiple functions this way.");
+//   });
+
+
+// returns a promise
 let countValue = new Promise(function (resolve, reject) {
-  resolve("Promise resolved");
+   reject('Promise rejected'); 
 });
 
 // executes when promise is resolved successfully
+countValue.then(
+    function successValue(result) {
+        console.log(result);
+    },
+ )
 
-countValue
-  .then(function successValue(result) {
-    console.log(result);
-  })
-
-  .then(function successValue1() {
-    console.log("You can call multiple functions this way.");
-  });
+// executes if there is an error
+.catch(
+    function errorValue(result) {
+        console.log(result);
+    }
+);
