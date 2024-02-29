65a4d6d80cb8d7a61cd78fabe4f3621de0171f73
Dinesh Kumar <dyyayavar7491@gmail.com>
2024-02-29 23:23:00 +0530

    display time every five second

--- 08.js
+++ 08.js
@@ -3658,8 +3658,23 @@ let data=[
 
 
 // program to display a text using setInterval method
-function greet() {
-    console.log('Hello world');
+// function greet() {
+//     console.log('Hello world');
+// }
+
+// setInterval(greet, 1000);
+
+
+// program to display time every 5 seconds
+function showTime() {
+
+    // return new date and time
+    let dateTime= new Date();
+
+    // return the time
+    let time = dateTime.toLocaleTimeString();
+
+    console.log(time)
 }
 
-setInterval(greet, 1000);
+let display = setInterval(showTime, 5000);
