a6802c3b2a6b7a640fbff2d6b4eeb8325e1b4283
Dinesh Kumar <dyyayavar7491@gmail.com>
2024-03-01 22:22:19 +0530

    Use clearInterval() Method

--- 08.js
+++ 08.js
@@ -3665,16 +3665,43 @@ let data=[
 // setInterval(greet, 1000);
 
 
-// program to display time every 5 seconds
-function showTime() {
+// // program to display time every 5 seconds
+// function showTime() {
 
-    // return new date and time
+//     // return new date and time
+//     let dateTime= new Date();
+
+//     // return the time
+//     let time = dateTime.toLocaleTimeString();
+
+//     console.log(time)
+// }
+
+// let display = setInterval(showTime, 5000);
+
+
+
+// program to stop the setInterval() method after five times
+
+let count = 0;
+
+// function creation
+let interval = setInterval(function(){
+
+    // increasing the count by 1
+    count += 1;
+
+    // when count equals to 5, stop the function
+    if(count === 5){
+        clearInterval(interval);
+    }
+
+    // display the current time
     let dateTime= new Date();
-
-    // return the time
     let time = dateTime.toLocaleTimeString();
+    console.log(time);
 
-    console.log(time)
-}
+}, 2000);
 
-let display = setInterval(showTime, 5000);
+
+
