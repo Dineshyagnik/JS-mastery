86acf18a1047982dd83aaa6d5606cd21b8ccffa7
Dinesh Kumar <dyyayavar7491@gmail.com>
2024-03-04 23:06:23 +0530

    date and time

--- 08.js
+++ 08.js
@@ -3729,11 +3729,31 @@ let data=[
 
 // console.log(dataa.dataa2[0]);
 
-const time1 = new Date(0);
+// const time1 = new Date(0);
 
-// epoch time
-console.log(time1);
+// // epoch time
+// console.log(time1);
 
-// 100000000000 milliseconds after the epoch time
-const time2 = new Date(100000000000)
-console.log(time2);
+// // 100000000000 milliseconds after the epoch time
+// const time2 = new Date(100000000000)
+// console.log(time2);
+
+// short date format "MM/DD/YYYY"
+const date = new Date("03/25/2015");
+console.log(date); 
+
+// long date format "MMM DD YYYY"
+const date1 = new Date("Jul 1 2020");
+console.log(date1); 
+
+// month and day can be in any order
+const date2 = new Date("1 Jul 2020");
+console.log(date2); 
+
+// month can be full or abbreviated. Also month names are insensitive.
+// comma are ignored
+const date3 = new Date("July 1 2020");
+console.log(date3);
+
+const date4 = new Date("JULY, 1, 2020");
+console.log(date4); 
