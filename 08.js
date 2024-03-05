3c8dab1b3747b617f0941aa9ce1c6f184a2f0321
Dinesh Kumar <dyyayavar7491@gmail.com>
2024-03-05 23:05:59 +0530

    closures

--- 08.js
+++ 08.js
@@ -3738,22 +3738,35 @@ let data=[
 // const time2 = new Date(100000000000)
 // console.log(time2);
 
-// short date format "MM/DD/YYYY"
-const date = new Date("03/25/2015");
-console.log(date); 
+// // short date format "MM/DD/YYYY"
+// const date = new Date("03/25/2015");
+// console.log(date); 
 
-// long date format "MMM DD YYYY"
-const date1 = new Date("Jul 1 2020");
-console.log(date1); 
+// // long date format "MMM DD YYYY"
+// const date1 = new Date("Jul 1 2020");
+// console.log(date1); 
 
-// month and day can be in any order
-const date2 = new Date("1 Jul 2020");
-console.log(date2); 
+// // month and day can be in any order
+// const date2 = new Date("1 Jul 2020");
+// console.log(date2); 
 
-// month can be full or abbreviated. Also month names are insensitive.
-// comma are ignored
-const date3 = new Date("July 1 2020");
-console.log(date3);
+// // month can be full or abbreviated. Also month names are insensitive.
+// // comma are ignored
+// const date3 = new Date("July 1 2020");
+// console.log(date3);
 
-const date4 = new Date("JULY, 1, 2020");
-console.log(date4); 
+// const date4 = new Date("JULY, 1, 2020");
+// console.log(date4); 
+
+
+
+
+// nested function example
+function greet(name) {
+    function displayName() {
+        console.log('Hi' + ' ' + name);
+    }
+    displayName();
+}
+
+greet('John'); 
