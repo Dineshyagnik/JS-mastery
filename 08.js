8e19317e1cc2f76b4758536d8e7a21c4430366e0
Dinesh Kumar <dyyayavar7491@gmail.com>
2024-03-10 22:08:26 +0530

    this keyword

--- 08.js
+++ 08.js
@@ -3805,18 +3805,25 @@ let data=[
 
 // closure example
 
-function calculate(x) {
-    function multiply(y) {
-        return x * y;
-    }
-    return multiply;
-}
+// function calculate(x) {
+//     function multiply(y) {
+//         return x * y;
+//     }
+//     return multiply;
+// }
 
-const multiply3 = calculate(3);
-const multiply4 = calculate(4);
+// const multiply3 = calculate(3);
+// const multiply4 = calculate(4);
 
-console.log(multiply3); 
-console.log(multiply3()); 
+// console.log(multiply3); 
+// console.log(multiply3()); 
 
-console.log(multiply3(6)); 
-console.log(multiply4(2)); 
+// console.log(multiply3(6)); 
+// console.log(multiply4(2)); 
+
+
+let a = this;
+console.log(a);
+
+this.name = 'Sarah';
+console.log(window.name); 
