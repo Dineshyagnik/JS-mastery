d28882faab3e744ec3eded44484e260dea4ef83d
Dinesh Kumar <101325099+Dineshyagnik@users.noreply.github.com>
2024-04-01 14:02:39 +0530

    filter in js

--- 08.js
+++ 08.js
@@ -4140,11 +4140,32 @@ let data=[
 
 
 
-function init() {
-  var name = "Mozilla"; 
-  function displayName() {
-    console.log(name); 
+// function init() {
+//   var name = "Mozilla"; 
+//   function displayName() {
+//     console.log(name); 
+//   }
+//   displayName();
+// }
+// init();
+
+
+// const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
+
+// const result = words.filter((word) => word.length > 6);
+
+// console.log(result);
+
+
+const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+function isPrime(num) {
+  for (let i = 2; num > i; i++) {
+    if (num % i === 0) {
+      return false;
+    }
   }
-  displayName();
+  return num > 1;
 }
-init();
+
+console.log(array.filter(isPrime)); // [2, 3, 5, 7, 11, 13]
