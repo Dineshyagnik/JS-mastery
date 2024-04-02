060903aece51a8021e9d099b48ffbf93c6dc89e9
Dinesh Kumar <101325099+Dineshyagnik@users.noreply.github.com>
2024-04-02 18:04:20 +0530

    function js

--- 08.js
+++ 08.js
@@ -4157,15 +4157,40 @@ let data=[
 // console.log(result);
 
 
-const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+// const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
-function isPrime(num) {
-  for (let i = 2; num > i; i++) {
-    if (num % i === 0) {
-      return false;
-    }
-  }
-  return num > 1;
+// function isPrime(num) {
+//   for (let i = 2; num > i; i++) {
+//     if (num % i === 0) {
+//       return false;
+//     }
+//   }
+//   return num > 1;
+// }
+
+// console.log(array.filter(isPrime)); // [2, 3, 5, 7, 11, 13]
+
+
+// function myFunc(theObject) {
+//   theObject.make = "Toyota";
+// }
+
+// const mycar = {
+//   make: "Honda",
+//   model: "Accord",
+//   year: 1998,
+// };
+
+// console.log(mycar.make); // "Honda"
+// myFunc(mycar);
+// console.log(mycar.make); // "Toyota"
+
+function myFunc(theArr) {
+  theArr[0] = 30;
 }
 
-console.log(array.filter(isPrime)); // [2, 3, 5, 7, 11, 13]
+const arr = [45];
+
+console.log(arr[0]); // 45
+myFunc(arr);
+console.log(arr[0]); // 30
